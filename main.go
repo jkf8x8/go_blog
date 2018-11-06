3c2212ce9d9a36cb44bc5e2cb11138cadaaa4860
hx_6 <jkf8x8_6@163.com>
2018-11-06 17:32:01 +0800

    搭建 1

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"go_blog/controllers"
+)
+
+func main(){
+
+	ctl.Run()
+	fmt.Println("ok")
+
+}
---
+++ routers/router.go
@@ -0,0 +1,15 @@
+package route
+
+import (
+	"fmt"
+	"net/http"
+	"github.com/julienschmidt/httprouter"
+)
+
+func init(){
+
+}
+
+func Run(){
+	
+}
